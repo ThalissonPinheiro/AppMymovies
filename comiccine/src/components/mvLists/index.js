7e8643afb3905e7158f7023df3b04319079e44b1
Thalisson Pinheiro <thalissontec2023@gmail.com>
2024-04-05 05:22:38 +0000

    T004: feat - Finalizado o componente mvLists

--- comiccine/src/components/mvLists/index.js
+++ comiccine/src/components/mvLists/index.js
@@ -1,15 +1,29 @@
 import React from "react";
 import { ScrollView } from "react-native";
+import styles from "./styles";
 import MvCard from "../mvCard";
 import { MOVIES } from "../../dataset/movies";
 
-const MvLists = () => {
+const MvLists = ({ navigation }) => {
 
+    const navegar = (filmes) => {
+        navigation.navigate('Details', { filmes });
+    }
     return (
-        <ScrollView>
+        <ScrollView
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            style={styles.horizontal.testee}
+        >
             {
                 MOVIES.map(
-                    (movie) => <MvCard movie={movie}/> 
+                    (movie) => {
+                        return <MvCard
+                            key={movie.title}
+                            movie={movie}
+                            onPress={() => navegar(movie)}
+                        />
+                    }
                 )
             }
         </ScrollView>
