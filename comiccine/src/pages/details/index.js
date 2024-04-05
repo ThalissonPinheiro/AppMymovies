e27672879f7463563604de2044c7f8a69fac08e8
Thalisson Pinheiro <thalissontec2023@gmail.com>
2024-04-05 05:26:49 +0000

    T004: feat - Teste da chegada de dados do dataset a tela details

--- comiccine/src/pages/details/index.js
+++ comiccine/src/pages/details/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Text, ScrollView, Image } from 'react-native';
+import { MOVIES_DETAILS } from '../../dataset/movies';
+import styles from './styles';
 
 const Details = ({ route }) => {
+    const { filmes } = route.params;
+    const movie = MOVIES_DETAILS.find(
+        movie => movie.title === filmes.title);
+
+    console.log(movie);
+
     return (
-        <>
-        </>
+        <ScrollView>
+            <Image
+                style={styles.img}
+                source={{ uri: movie.backdrop_path}}
+            />
+
+            <Text style={styles.titulo}>
+                {movie.title}
+            </Text>
+
+        </ScrollView>
     );
 }
 
