23eadcf998a3c069b213284b6bef0be38340b46f
Thalisson Pinheiro <thalissontec2023@gmail.com>
2024-04-06 12:40:41 +0000

    T005: feat - Finalizado por hora a page Details

--- comiccine/src/pages/details/index.js
+++ comiccine/src/pages/details/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, ScrollView, Image } from 'react-native';
+import { Text, ScrollView, Image, View } from 'react-native';
 import { MOVIES_DETAILS } from '../../dataset/movies';
 import styles from './styles';
 
@@ -8,17 +8,32 @@ const Details = ({ route }) => {
     const movie = MOVIES_DETAILS.find(
         movie => movie.title === filmes.title);
 
-    console.log(movie);
-
     return (
-        <ScrollView>
+        <ScrollView style={styles.back}>
+            <Text style={styles.vie}>
+                👁{movie.popularity}
+            </Text>
+            <Text style={styles.star}>
+                ⭐{movie.vote_average}
+            </Text>
+
             <Image
                 style={styles.img}
-                source={{ uri: movie.backdrop_path}}
+                source={{ uri: movie.backdrop_path }}
             />
-
+            <View style={styles.overlay}></View>
+            <Image
+                style={styles.card}
+                source={{
+                    uri: movie.poster_path
+                }}
+            />
             <Text style={styles.titulo}>
                 {movie.title}
+            </Text>
+            <Text style={{ color: '#D8315B', marginLeft: '12%', marginTop: 10, fontSize: 20, fontWeight: 'bold', }}>Sinopse</Text>
+            <Text style={styles.sub}>
+                {movie.overview}
             </Text>
 
         </ScrollView>
--- comiccine/src/pages/details/styles.js
+++ comiccine/src/pages/details/styles.js
@@ -1,17 +1,68 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
+star: {
+    top: '21%',
+    left: '75%',
+    position: 'absolute',
+    zIndex: 2,
+    fontSize: 14,
+    color: 'gold',
+},
+vie: {
+    top: '16%',
+    left: '75%',
+    position: 'absolute',
+    zIndex: 2,
+    fontSize: 14,
+    color: 'white',
+},
+back: {
+    backgroundColor: '#111111',
+},
 img: {
-    marginTop: 20,
+    width: 400,
+    height: 300,
+    zIndex: 0,
+},
+overlay: {
+    width: 400,
+    height: 300,
+    position: 'absolute',
+    zIndex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    ...StyleSheet.absoluteFillObject,
+},
+titulo: {
     alignSelf: 'center',
-    width: 350,
-    height: 250,
-},   
-titulo:{
+    zIndex: 1,
     textAlign: 'center',
-    marginTop: 20,
+    marginTop: 35,
     fontSize: 20,
-    fontFamily: 'arial black'
+    fontFamily: 'arial black',
+    color: '#D8315B',
+},
+card: {
+    width: 170,
+    height: 240,
+    position: 'absolute',
+    zIndex: 1,
+    marginTop: 90,
+    alignSelf: 'center',
+},
+sub: {
+    color: 'white',
+    alignSelf: 'center',
+    marginTop: 8,
+    zIndex: 1,
+    fontSize: 16,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    borderRadius: 5,
+    width: 300,
+    height: 'auto',
+    textAlign: 'justify',
+    padding: 10,
 },
 
 })
